fa8afc8952d6cafd64540f4c54e057019cf78fe4
root <root@mine.pangkalpinang.site>
2021-11-20 03:24:40 +0000

    Update README.md

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+COMMON_PATH="$(cd "$(dirname "$0")/../" >/dev/null 2>&1 && pwd)"
+#COMMON_PATH="/root"
+KERNEL_PATH="$(cd "$(dirname "$0")" >/dev/null 2>&1 && pwd)"
+
+export PATH=$COMMON_PATH/proton-clang/bin/:$COMMON_PATH/aarch64-linux-android/bin/:${PATH}
+export ARCH=arm64
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-android-
+export LD_LIBRARY_PATH=$COMMON_PATH/proton-clang/lib
+
+cd $KERNEL_PATH
+make -j$(nproc --all) CC=clang $@
